faebc0fafdabe0fb46df9ebcc49261c3bb238106
bastidio <101216960+bastidio@users.noreply.github.com>
2026-04-27 11:16:12 -0300

    .

--- src/Iteracion/Paquete.java
+++ src/Iteracion/Paquete.java
@@ -1,24 +1,48 @@
 package Iteracion;
 
+
 public class Paquete<T> {
 	private String id;
 	private double peso;
 	private String destino;
 	private boolean urgente;
 	private T contenido;
+	private String idHash;
+
 	
 	public Paquete(String id, double peso, String destino, boolean urgente, T contenido) {
+		if (id == null || id.trim().isEmpty()) {
+            throw new IllegalArgumentException("El ID no puede ser nulo o vacío.");
+        }
+        if (destino == null || destino.trim().isEmpty()) {
+            throw new IllegalArgumentException("El destino no puede ser nulo o vacío.");
+        }
+        if (contenido == null) {
+            throw new IllegalArgumentException("El contenido no puede ser nulo.");
+        }
+        if (peso <= 0) {
+            throw new IllegalArgumentException("El peso debe ser mayor a 0. Valor ingresado: " + peso);
+        }
+		
 		this.id = id;
 		this.peso = peso;
 		this.destino = destino;
 		this.urgente = urgente;
 		this.contenido = contenido;
+		this.idHash = generarHash(id);
 	}
+	
 
+	private String generarHash(String id) {
+        return Integer.toHexString(id.hashCode()).toUpperCase();
+    }
+	
 	public String getId() {
 		return id;
 	}
 
+	public String getIdHash() { return idHash; }
+	
 	public double getPeso() {
 		return peso;
 	}
