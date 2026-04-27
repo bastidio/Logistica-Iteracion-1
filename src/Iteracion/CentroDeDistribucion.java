8ec6d799f477cb717bf60528ce93fbf4aaf7c4a7
bastidio <101216960+bastidio@users.noreply.github.com>
2026-04-27 11:15:24 -0300

    hash

--- src/Iteracion/CentroDeDistribucion.java
+++ src/Iteracion/CentroDeDistribucion.java
@@ -1,14 +1,16 @@
 package Iteracion;
 
 import java.util.PriorityQueue;
+import java.util.HashSet;
 
 public class CentroDeDistribucion {
-	private PriorityQueue<Paquete<?>> filaPaquetes;
-	
-	public CentroDeDistribucion() {
+    private PriorityQueue<Paquete<?>> filaPaquetes;
+    private HashSet<String> controlDeIds; 
+    
+    public CentroDeDistribucion() {
+        this.controlDeIds = new HashSet<>();
         this.filaPaquetes = new PriorityQueue<>((p1, p2) -> {
             boolean p1EsPrioridad = p1.isUrgente() || p1.getPeso() > 50;
-            
             boolean p2EsPrioridad = p2.isUrgente() || p2.getPeso() > 50;
 
             if (p1EsPrioridad && !p2EsPrioridad) {
@@ -21,19 +23,23 @@ public class CentroDeDistribucion {
                 return 0; 
             }
         });
-	}
-        public void recibirPaquete(Paquete<?> nuevoPaquete) {
+    }
+
+    public void recibirPaquete(Paquete<?> nuevoPaquete) {
+        if (controlDeIds.add(nuevoPaquete.getId())) {
             filaPaquetes.add(nuevoPaquete);
             System.out.println("Paquete " + nuevoPaquete.getId() + " ingresado al sistema.");
+        } else {
+            throw new IllegalArgumentException("El ID '" + nuevoPaquete.getId() + "' ya está registrado en el sistema.");
         }
-        
-        public Paquete<?> procesarSiguientePaquete() {
-            if (!filaPaquetes.isEmpty()) {
-                return filaPaquetes.poll();
-            } else {
-                System.out.println("No hay paquetes para procesar.");
-                return null;
-            }
+    }
+    
+    public Paquete<?> procesarSiguientePaquete() {
+        if (!filaPaquetes.isEmpty()) {
+            return filaPaquetes.poll();
+        } else {
+            System.out.println("No hay paquetes para procesar.");
+            return null;
         }
+    }
 }
-
